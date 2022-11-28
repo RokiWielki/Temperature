083d97fae05ab1a0ad73c92fdae0074548ec2c07
RokiWielki <Cruck222@interia.pl>
2022-11-28 16:29:48 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Napisz program przeliczający temperaturę w stopniach Celsjusza na temperaturę w
+//stopniach Fahrenheita. Program ma prosić użytkownika o podanie temperatury w stopniach
+//Celsjusza. Wzór: F=32+(5/9)C.
+
+using System;
+
+namespace Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
